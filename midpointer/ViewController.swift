7c2aeb813184588c1fcd1bdd85dbb566e1448014
Jean de Klerk <jadekler@gmail.com>
2016-02-06 14:50:10 -0700

    Initial commit

---
+++ midpointer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  midpointer
+//
+//  Created by Jean de Klerk on 2/6/16.
+//  Copyright © 2016 Jean de Klerk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
