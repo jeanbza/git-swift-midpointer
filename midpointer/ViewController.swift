9485053086e2d496b2a2cb6e4c48cd731b775157
Jean de Klerk <jadekler@gmail.com>
2016-02-09 22:41:33 -0700

    Small cleanups

--- midpointer/ViewController.swift
+++ midpointer/ViewController.swift
@@ -25,16 +25,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-    // MARK: - Actions
-    
+
     @IBAction func reset(sender: AnyObject) {
         mainImageView.image = nil
     }
